b80decef5cfa0d82283761f367b8454594cd9ea5
Rodrigo Viana <rodrigomourav@gmail.com>
2021-12-13 15:45:06 -0300

    Autenticação corrigidda

--- Configurando.R
+++ Configurando.R
@@ -6,3 +6,13 @@ usethis::use_git_config(user.email = "rodrigomourav@gmail.com",
 usethis::create_project("C:/Users/zellar/OneDrive/MBA/Data Wrangling/R com GIT")
 
 usethis::use_git()
+
+##usethis::create_github_token()
+##usethis::gh_token_help()
+
+##Digitar a senha (ou token criado acima)
+gitcreds::gitcreds_set()
+
+usethis::use_github()
+
+usethis::use_readme_md()
---
+++ README.md
@@ -0,0 +1,6 @@
+
+# R com GIT
+
+<!-- badges: start -->
+<!-- badges: end -->
+teste
