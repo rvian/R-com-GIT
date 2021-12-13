c74eeb2997ae455ca868eddf0b02b6e08f478685
Rodrigo Viana <rodrigomourav@gmail.com>
2021-12-13 14:51:11 -0300

    Initial commit

---
+++ Configurando.R
@@ -0,0 +1,8 @@
+install.packages("use_this")
+
+usethis::use_git_config(user.email = "rodrigomourav@gmail.com",
+                        user.name=  "Rodrigo Viana")
+
+usethis::create_project("C:/Users/zellar/OneDrive/MBA/Data Wrangling/R com GIT")
+
+usethis::use_git()
